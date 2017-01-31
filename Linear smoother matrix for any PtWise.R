fc24bd7df753a6addbbf367734e29efe7aeb5c45
hellosheng1989 <sishengliu1989@gmail.com>
2017-01-31 11:54:40 -0500

    Update Linear smoother matrix for any PtWise.R

--- Linear smoother matrix for any PtWise.R
+++ Linear smoother matrix for any PtWise.R
@@ -2,7 +2,6 @@ source("CPE with any PtWise.R")
 ###### G_D1Matrix calculate the linear smoother for compound estimation of 1st derivative
 #####Yvec ~ Xvec
 #####polyOd is the order J we want for pointwise estimators.
-####xvec is 
 G_D1Matrix = function(PtMat=PtMat,beta_nVec=beta_nVec,aVec=aVec,Yvec=Yvec,Xvec=Xvec,polyOd=polyOd,Svec=Svec){
   n=length(Yvec)
   TempYmat = diag(n)
